634873b6be334935dbb912262fcb74e277beddc7
hulsed <hulsed@oregonstate.edu>
2017-07-21 15:02:58 -0700

    fixed CO approach to standard approach (optimizing shared variables as well as local)

--- CO_Propane/opt_ss1.m
+++ CO_Propane/opt_ss1.m
@@ -1,25 +1,22 @@
 function J1=opt_ss1(z)
 
 K5=1;
-initpt=[z(2),z(4)];
+initpt=[z];
+LB=zeros(1,11);
 options = optimset('Display', 'off') ;
-LB=[0,0];
+LB=zeros(1,11);
 [xopt,J1,exitflag,output]=fmincon(@ss1_obj,initpt,[],[],[],[],LB,[],@ss1_con,options);
 
     function obj=ss1_obj(x)
-        x2=x(1);
-        x4=x(2);
-        
-        obj=(x2-z(2))^2+(x4-z(4))^2;
+        obj=sum((z-x).^2);
         
     end
 
     function [cineq,ceq]=ss1_con(x)
-        x2=x(1);
-        x4=x(2);
         
-        f1=z(1)+x4-3;
-        f5=K5*x2*x4-z(1)*z(5);
+        
+        f1=x(1)+x(4)-3;
+        f5=K5*x(2)*x(4)-x(1)*x(5);
         
         ceq=[f1,f5];
         cineq=[];
--- CO_Propane/opt_ss2.m
+++ CO_Propane/opt_ss2.m
@@ -3,16 +3,13 @@ function J2=opt_ss2(z)
 K8=0.1;
 K10=0.1;
 P=40;
-initpt=[z(8),z(10)];
-LB=[0,0];
+initpt=[z];
+LB=zeros(1,11);
 options = optimset('Display', 'off') ;
 [xopt,J2,exitflag,output]=fmincon(@ss2_obj,initpt,[],[],[],[],LB,[],@ss2_con,options);
 
     function obj=ss2_obj(x)
-        x8=x(1);
-        x10=x(2);
-        
-        obj=(x8-z(8))^2+(x10-z(10))^2;
+        obj=sum((z-x).^2);
         
     end
 
@@ -20,8 +17,8 @@ options = optimset('Display', 'off') ;
         x8=x(1);
         x10=x(2);
         
-        f8=K8*z(1)-z(4)*x8*P/z(11);
-        f10=K10*z(1)^2-z(4)^2*x10*P/z(11);
+        f8=K8*x(1)-x(4)*x(8)*P/x(11);
+        f10=K10*x(1)^2-x(4)^2*x(10)*P/x(11);
         
         ceq=[f8,f10];
         cineq=[];
